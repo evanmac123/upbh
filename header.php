16a2f16cc31c689a8b2fa93e1bab82f38190b16f
Evan  McEldowney <evanmceldowney@Evans-MacBook-Pro.local>
2017-10-08 22:38:08 -0600

    formatting up

--- header.php
+++ header.php
@@ -17,6 +17,23 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+		<!-- Google Analytics Code Begins -->
+		<script>
+				(function (i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments)
+		}, i[r].l = 1 * new Date();
+		a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-39767596-1', 'auto');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
+		</script>
+				<!-- Google Analytics Code Ends -->
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
@@ -36,7 +53,15 @@
 		<nav class="site-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php
+						// Display the Custom Logo, default logo, or the site's name
+						if ( has_custom_logo() ) {
+                            the_custom_logo();
+                        } else { ?>
+                            <img width="130" height="130" src="http://www.muirwoodteen.com/wp-content/uploads/2017/07/cropped-logo-1-3.png" class="custom-logo" alt="Muir Wood Logo" itemprop="logo" />
+                        <?php } ?>
+                    </a>
 				</div>
 			</div>
 			<div class="top-bar-right">
@@ -47,7 +72,7 @@
 				<?php endif; ?>
 			</div>
 		</nav>
-		
+
 	</header>
 
 	<div class="container">
